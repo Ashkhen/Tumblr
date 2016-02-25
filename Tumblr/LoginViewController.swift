16d08449f1e79f0a6e49b9bd4cfd20a85067b744
Ashkhen Sargsyan <ashi@a5.io>
2016-02-25 15:21:59 -0800

    - Added a login view controller. Login button from home view controller modally presents the login view controller. Dimsisses on cancel. - HomeViewController: Cleaned up code

--- Tumblr/LoginViewController.swift
+++ Tumblr/LoginViewController.swift
@@ -1,17 +1,20 @@
 //
-//  HomeViewController.swift
+//  LoginViewController.swift
 //  Tumblr
 //
-//  Created by Ashkhen Sargsyan on 2/24/16.
+//  Created by Ashkhen Sargsyan on 2/25/16.
 //  Copyright © 2016 Ashkhen Sargsyan. All rights reserved.
 //
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var loginFormImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
      
     }
 
@@ -19,5 +22,8 @@ class HomeViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
- 
+    
+    @IBAction func onCancelTap(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
 }
