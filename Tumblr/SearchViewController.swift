d9b9567bea16f5d3ede19eef85d63cc3f6f45c9d
Ashkhen Sargsyan <ashi@a5.io>
2016-02-27 14:48:08 -0800

    - Added loading animation on search.

--- Tumblr/SearchViewController.swift
+++ Tumblr/SearchViewController.swift
@@ -10,10 +10,38 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    @IBOutlet weak var searchFeedImage: UIImageView!
+    @IBOutlet weak var loadingImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        searchFeedImage.hidden = true
+        loadingImage.hidden = false
+        loadingImage.image = UIImage(named: "loading-1")
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        var images: [UIImage] = []
+        for i in 1...3 {
+            images.append(UIImage(named: "loading-\(i)")!)
+        }
+        
+        loadingImage.animationImages = images
+        loadingImage.animationDuration = 1.99
+        loadingImage.startAnimating()
+        
+        delay(2) { () -> () in
+            self.searchFeedImage.hidden = false
+            self.loadingImage.hidden = true
+            self.loadingImage.stopAnimating()
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +49,12 @@ class SearchViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func delay(delay:Double, closure:()->()) {
+        dispatch_after(
+            dispatch_time(
+                DISPATCH_TIME_NOW,
+                Int64(delay * Double(NSEC_PER_SEC))
+            ),
+            dispatch_get_main_queue(), closure)
     }
-    */
-
 }
